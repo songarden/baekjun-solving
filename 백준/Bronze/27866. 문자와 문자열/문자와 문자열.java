2b055b5f76a420a57fec5a2bfadc4ed0a4095aaf
Son Jung Won <102501352+songarden@users.noreply.github.com>
2023-10-14 16:00:21 +0900

    [Bronze V] Title: 문자와 문자열, Time: 212 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        int n = sc.nextInt();
+        System.out.println(word.charAt(n-1));
+    }
+}
