c3d4cbf8e010afc46754fe63d9eb725d14aa291d
Son Jung Won <102501352+songarden@users.noreply.github.com>
2023-10-18 17:09:25 +0900

    [Silver I] Title: 곱셈, Time: 68 ms, Memory: 34512 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,24 @@
+from collections import deque
+import sys
+import heapq
+
+a,b,c = map(int,sys.stdin.readline().strip().split())
+
+# while a <= c and b>=0:
+#     a *= a
+#     b -= 1
+# result = a%c
+# for i in range(b):
+#     result = (result*a)%c
+
+def div_mup(a,b):
+    if b == 1:
+        return a%c
+    elif b%2 == 1:
+        return div_mup(a, b//2)**2*a%c
+    else:
+        return div_mup(a, b//2)**2%c
+result = div_mup(a,b)
+
+
+print(result)
