5aa09d53c75bed2e59d6a37696f74cf249d83d95
Son Jung Won <102501352+songarden@users.noreply.github.com>
2023-10-21 15:34:00 +0900

    [Gold III] Title: 아침 산책, Time: 1300 ms, Memory: 122540 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,7 +1,7 @@
 from collections import deque
 import sys
 import heapq
-sys.setrecursionlimit(10**8)
+sys.setrecursionlimit(10**6)
 
 n = int(sys.stdin.readline())
 tree = {}
@@ -9,33 +9,43 @@ tree = {}
 isInside = list(map(int,sys.stdin.readline().strip()))
 visited = [False for _ in range(n+1)]
 count = 0
-for i in range(n-1):
-    u , v = map(int,sys.stdin.readline().strip().split())
-    if not u in tree:
-        tree[u] = [v]
-    else:
-        tree[u].append(v)
-    if not v in tree:
-        tree[v] = [u]
-    else:
-        tree[v].append(u)
+while True:
+    try:
+        u , v = map(int,sys.stdin.readline().strip().split())
+        if not u in tree:
+            tree[u] = [v]
+        else:
+            tree[u].append(v)
+        if not v in tree:
+            tree[v] = [u]
+        else:
+            tree[v].append(u)
+    except:
+        break
 
 
 
-def dfs_walk(v):
-    global count
+def dfs(v):
+    inside_count = 0
     visited[v] = True
     for i in tree[v]:
         if not visited[i]:
             if isInside[i-1]:
-                count += 1
+                inside_count += 1
                 continue
             else :
-                dfs_walk(i)
+                inside_count += dfs(i)
+    return inside_count
 
 for i in tree.keys():
-    if isInside[i-1]:
-        dfs_walk(i)
-        visited = [False for _ in range(n+1)]
+    if not isInside[i-1]:
+        if not visited[i]:
+            inside_count = dfs(i)
+            count += inside_count*(inside_count-1)
+    else:
+        for j in tree[i]:
+            if isInside[j-1]:
+                count +=1  
+
 
 print(count)
