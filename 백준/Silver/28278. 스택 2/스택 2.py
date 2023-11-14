af247b6dcb6a46198cef51476d20829d3b355e91
Son Jung Won <102501352+songarden@users.noreply.github.com>
2023-11-14 21:27:46 +0900

    [Silver IV] Title: 스택 2, Time: 1376 ms, Memory: 74720 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,27 @@
+from collections import deque
+import sys
+import math
+
+n = int(sys.stdin.readline())
+stack = []
+for _ in range(n):
+    command = list(map(int,sys.stdin.readline().split()))
+    if command[0] == 1:
+        stack.append(command[1])
+    elif command[0] == 2:
+        if not stack:
+            print(-1)
+        else:
+            print(stack.pop())
+    elif command[0] == 3:
+        print(len(stack))
+    elif command[0] == 4:
+        if not stack:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 5:
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
